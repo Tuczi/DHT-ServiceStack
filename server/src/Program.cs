cc32b745eb5f90a772b7839c8e34c5b8787d3f39
Tuczi <tuczi007@gmail.com>
2016-02-13 12:49:00 +0000

    join/leave dht progress

--- server/src/Program.cs
+++ server/src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using RestSharp;
+using System.Collections.Generic;
 
 
 namespace Server
@@ -8,45 +9,72 @@ namespace Server
 	{
 		private static void Main ()
 		{
-			var parentServerUrl = Environment.GetEnvironmentVariable ("PARENT_URL");
+			var myPublicUrl = Environment.GetEnvironmentVariable ("PUBLIC_URL");//http://localhost:8888/
+			if (string.IsNullOrWhiteSpace (myPublicUrl) || !Uri.IsWellFormedUriString (myPublicUrl, UriKind.RelativeOrAbsolute))
+				throw new MissingFieldException ("PUBLIC_URL have to be set");
+			
+			var parentServerUrl = Environment.GetEnvironmentVariable ("PARENT_URL");//http://localhost:8889/
+			if (!string.IsNullOrWhiteSpace (parentServerUrl) && !Uri.IsWellFormedUriString (parentServerUrl, UriKind.RelativeOrAbsolute))
+				throw new FormatException ("PARENT_URL have to be URL");
+			
 			var appHost = new AppHost ();
 			appHost.Init ();
-			ushort port = 8888;
-			string listeningOn = string.Format ("http://*:{0}/", port);
-			appHost.Start (listeningOn);
+			Console.WriteLine (myPublicUrl);
+			appHost.Start (myPublicUrl);
 
-			Console.WriteLine ("AppHost Created at {0}, listening on {1}, parent server URL is {2}", DateTime.Now, listeningOn, parentServerUrl);
-			if(parentServerUrl != null)
-				joinDHT (parentServerUrl);
+			Console.WriteLine ("AppHost Created at {0}, listening on {1}, parent server URL is {2}", DateTime.Now, myPublicUrl, parentServerUrl);
+			joinDHT (parentServerUrl, myPublicUrl);
 
 			Console.WriteLine ("Press ENTER to exit...");
 			Console.ReadLine ();
-			if(parentServerUrl != null)
-				leaveDHT (parentServerUrl);
+			leaveDHT ();
 		}
 
 		//TODO find way to get context from app host - container field?
-		private static void joinDHT (string parentServerUrl)
+		private static void joinDHT (string parentServerUrl, string myPublicUrl)
 		{
+			if (string.IsNullOrEmpty (parentServerUrl)) {
+				Console.WriteLine ("Joined with code 200 - No parent");
+				return;
+			}
+			
 			var client = new RestClient (parentServerUrl);
-
 			var request = new RestRequest ("dht/join", Method.POST);
-			//request.AddBody ({"Child": "TODO"}); //TODO my addr in Child Field
+			request.RequestFormat = DataFormat.Json;
+			var data = new Dictionary<string, Object> ();
 
+			data.Add ("Child", myPublicUrl);
+
+			request.AddJsonBody (data);
+			Console.WriteLine ("Sending dht/join request to {0}", parentServerUrl);
 			RestResponse response = (RestResponse)client.Execute (request);
+
 			var content = response.Content;
 			//TODO add to DB
+			Console.WriteLine ("Joined with code {0}", response.StatusCode);
 		}
 
-		private static void leaveDHT (string parentServerUrl)
+		private static void leaveDHT ()
 		{
-			var client = new RestClient (parentServerUrl);
+			if (string.IsNullOrEmpty (DHTServerCtx.DHT.Parent)) {
+				Console.WriteLine ("Left with code 200 - No parent");
+				return;
+			}
 
+			var client = new RestClient (DHTServerCtx.DHT.Parent);
 			var request = new RestRequest ("dht/leave", Method.POST);
-			// request.AddBody //TODO my "Child", my "HashRange", my "Data" List<ValueDto>
+			request.RequestFormat = DataFormat.Json;
+			var data = new Dictionary<string, Object> ();
 
+			Console.WriteLine ("Sending dht/leave request to {0}", DHTServerCtx.DHT.Parent);
+			data.Add ("Child", DHTServerCtx.DHT.Child);
+			data.Add ("HashRange", DHTServerCtx.DHT.HashRange);
+			data.Add ("Data", "TODO");//TODO List<ValueDto>
+
+			request.AddJsonBody (data);
 			RestResponse response = (RestResponse)client.Execute (request);
-			//TODO check response status
+
+			Console.WriteLine ("Left with code {0}", response.StatusCode);
 		}
 	}
 }
