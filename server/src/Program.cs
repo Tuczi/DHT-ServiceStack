70579faae59b05628691bfd31a65a5ec1786f601
Tuczi <tuczi007@gmail.com>
2016-02-15 16:21:25 +0000

    dht join/leave finished

--- server/src/Program.cs
+++ server/src/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using Funq;
 using RestSharp;
 using RestSharp.Deserializers;
+using Server.Logic.Value;
 using Server.Services.ServerService;
 using Server.Services.ValueService;
-using System.Numerics;
-
+using ServiceStack.OrmLite;
 
 namespace Server
 {
@@ -27,16 +31,16 @@ namespace Server
 			appHost.Start (myPublicUrl);
 
 			Console.WriteLine ("AppHost Created at {0}, listening on {1}, parent server URL is {2}", DateTime.Now, myPublicUrl, parentServerUrl);
-			joinDHT (parentServerUrl, myPublicUrl);
+			joinDHT (parentServerUrl, myPublicUrl, appHost.Container);
 
 			Console.WriteLine ("Hash From {0} to {1}. Child {2} parent {3}", DHTServerCtx.DHT.HashRange.Min, DHTServerCtx.DHT.HashRange.Max, DHTServerCtx.DHT.Child, DHTServerCtx.DHT.Parent);
 			Console.WriteLine ("Press ENTER to exit...");
 			Console.ReadLine ();
-			leaveDHT ();
+			leaveDHT (appHost.Container);
 		}
 
 		//TODO find way to get context from app host - container field?
-		private static void joinDHT (string parentServerUrl, string myPublicUrl)
+		private static void joinDHT (string parentServerUrl, string myPublicUrl, Container container)
 		{
 			if (string.IsNullOrEmpty (parentServerUrl)) {
 				Console.WriteLine ("Joined with code 200 - No parent");
@@ -58,13 +62,27 @@ namespace Server
 
 			DHTServerCtx.DHT.Parent = parentServerUrl;
 			DHTServerCtx.DHT.Child = response.Data.Child;
-			DHTServerCtx.DHT.HashRange.Min = BigInteger.Parse(response.Data.RangeMin);
-			DHTServerCtx.DHT.HashRange.Max = BigInteger.Parse(response.Data.RangeMax);
-			//TODO add responseData.Data to DB
+			DHTServerCtx.DHT.HashRange.Min = BigInteger.Parse (response.Data.RangeMin);
+			DHTServerCtx.DHT.HashRange.Max = BigInteger.Parse (response.Data.RangeMax);
+
+			insertIntoDb (response.Data.Data, container);
 			Console.WriteLine ("Joined with code {0}", response.StatusCode);
 		}
 
-		private static void leaveDHT ()
+		private static void insertIntoDb (List<ValueDtoResponse> data, Container container)
+		{
+			var dbFactory = container.Resolve<IDbConnectionFactory> ();
+			var valueList = data.Select (v => new Value {
+				Key = v.Key,
+				Data = v.Data,
+				Hash = DHTServerCtx.HashFunction.apply (v.Key)
+			}).ToList ();
+			using (IDbConnection db = dbFactory.Open ()) {
+				db.InsertAll<Value> (valueList);
+			}
+		}
+
+		private static void leaveDHT (Container container)
 		{
 			if (string.IsNullOrEmpty (DHTServerCtx.DHT.Parent)) {
 				Console.WriteLine ("Left with code 200 - No parent");
@@ -75,16 +93,32 @@ namespace Server
 			var request = new RestRequest ("dht/leave", Method.POST);
 			request.RequestFormat = DataFormat.Json;
 			var data = new LeaveDto {Child = DHTServerCtx.DHT.Child,
-				RangeMin = DHTServerCtx.DHT.HashRange.Min.ToString(),
-				RangeMax = DHTServerCtx.DHT.HashRange.Max.ToString(),
-				Data = new List<ValueDto> ()
-			};//TODO read Data from DB
+				RangeMin = DHTServerCtx.DHT.HashRange.Min.ToString (),
+				RangeMax = DHTServerCtx.DHT.HashRange.Max.ToString (),
+				Data = removeFromDb (container)
+			};
 			request.AddJsonBody (data);
 
 			Console.WriteLine ("Sending dht/leave request to {0}", DHTServerCtx.DHT.Parent);
 			RestResponse response = (RestResponse)client.Execute (request);
 
 			Console.WriteLine ("Left with code {0}", response.StatusCode);
+		}
+
+		private static List<ValueDto> removeFromDb (Container container)
+		{
+			var dbFactory = container.Resolve<IDbConnectionFactory> ();
+
+			var valueList = new List<Value> (0);
+			using (IDbConnection db = dbFactory.Open ()) {
+				valueList = db.Select<Value> ();
+				db.DeleteAll<Value> ();
+			}
+
+			return valueList.Select (v => new ValueDto {
+				Key = v.Key,
+				Data = v.Data
+			}).ToList ();
 		}
 	}
 }
