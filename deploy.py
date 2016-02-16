6dfe4321e5678ebbfeae53d820b894d1ab096c64
Mateusz Rybarski <rybarski.mateusz@gmail.com>
2016-02-16 23:30:04 +0100

    fixed script

--- deploy.py
+++ deploy.py
@@ -1,8 +1,8 @@
 import paramiko
 import time
 
-hosts = ['ec2-54-191-220-240.us-west-2.compute.amazonaws.com', 'ec2-54-191-56-131.us-west-2.compute.amazonaws.com',
-         'ec2-54-191-78-13.us-west-2.compute.amazonaws.com']
+hosts = ['ec2-54-201-232-78.us-west-2.compute.amazonaws.com', 'ec2-54-191-55-36.us-west-2.compute.amazonaws.com',
+         'ec2-54-201-229-151.us-west-2.compute.amazonaws.com']
 
 if __name__ == '__main__':
 
@@ -11,14 +11,16 @@ if __name__ == '__main__':
 
     print hosts[0]
     ssh.connect(hosts[0], username='ubuntu', key_filename='/Users/mateusz/Downloads/mbook.pem.txt')
-    ssh.exec_command('cd /dht-server/bin/Debug && export PUBLIC_URL="http://%s:8888/" && screen -dmS app mono server.exe' % hosts[0])
+    ssh.exec_command(
+        'cd /dht-server/bin/Debug && export PUBLIC_URL="http://%s:8888/" && screen -dmS app mono server.exe' % hosts[0])
     ssh.close()
 
     for i in range(1, len(hosts)):
         print hosts[i]
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hosts[0], username='ubuntu', key_filename='/Users/mateusz/Downloads/mbook.pem.txt')
+        ssh.connect(hosts[i], username='ubuntu', key_filename='/Users/mateusz/Downloads/mbook.pem.txt')
+
         command = 'cd /dht-server/bin/Debug && export PUBLIC_URL="http://%s:8888/" && export PARENT_URL="http://%s:8888/" && screen -dmS app mono server.exe' % (
         hosts[i], hosts[i - 1])
         print command
