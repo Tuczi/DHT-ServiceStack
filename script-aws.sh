4d61a26005b84f5d83502d1ed7710fbc0d882b09
Mateusz Rybarski <rybarski.mateusz@gmail.com>
2016-02-16 22:32:08 +0100

    skrypt

--- script-aws.sh
+++ script-aws.sh
@@ -3,4 +3,6 @@ mkdir dht-server
 cd dht-server
 aws s3 cp s3://ryba-web-app/dht-aws.tar.gz .
 tar xvf dht-aws.tar.gz
-screen -dmS app mono bin/Debug/server.exe
+cd bin/Debug
+sudo chmod 777 .
+# screen -dmS app mono bin/Debug/server.exe
